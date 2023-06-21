c65c1bfa17bd5ed83f5f3e218e98560f55018fdc
Nilinz <greyhort@gmail.com>
2023-06-21 19:01:31 +0300

    Update hw11.py

--- hw11.py
+++ hw11.py
@@ -1,5 +1,5 @@
-import datetime
 from collections import UserDict
+from datetime import datetime, timedelta
 
 class Field:
     def __init__(self):
@@ -32,7 +32,7 @@ class Phone(Field):
     @Field.value.setter
     def value(self, new_value):
         if not new_value.isdigit():
-            raise ValueError("Phone number should only contain digits.")
+            raise ValueError("Invalid phone number.")
         self._value = new_value
 
 class Birthday(Field):
@@ -44,30 +44,20 @@ class Birthday(Field):
     def value(self, new_value):
         if new_value is not None:
             try:
-                datetime.datetime.strptime(new_value, "%Y-%m-%d")
+                datetime.strptime(new_value, "%Y-%m-%d")
             except ValueError:
                 raise ValueError("Invalid birthday format. Please use YYYY-MM-DD.")
         self._value = new_value
 
     def days_to_birthday(self):
-        if self.value is None:
-            return None
-
-        today = datetime.date.today()
-        next_birthday_year = today.year
-        next_birthday = datetime.datetime.strptime(
-            f"{next_birthday_year}-{self.value[5:]}",
-            "%Y-%m-%d"
-        ).date()
-
-        if today > next_birthday:
-            next_birthday_year += 1
-            next_birthday = datetime.datetime.strptime(
-                f"{next_birthday_year}-{self.value[5:]}",
-                "%Y-%m-%d"
-            ).date()
-
-        return (next_birthday - today).days
+        if self.value:
+            today = datetime.today()
+            birthday = datetime.strptime(self.value, "%Y-%m-%d")
+            next_birthday = datetime(today.year, birthday.month, birthday.day)
+            if next_birthday < today:
+                next_birthday = datetime(today.year + 1, birthday.month, birthday.day)
+            days = (next_birthday - today).days
+            return days
 
 class Record:
     def __init__(self, name, birthday=None):
@@ -86,21 +76,29 @@ class Record:
             if phone.value == old_phone:
                 phone.value = new_phone
 
+    def days_to_birthday(self):
+        return self.birthday.days_to_birthday()
+
 class AddressBook(UserDict):
     def __init__(self):
         super().__init__()
-        self.page_size = 5
+        self._index = 0
+        self._page_size = 5
 
     def add_record(self, record):
         self.data[record.name.value] = record
 
-    def iterator(self, page=1):
-        total_pages = len(self) // self.page_size + 1
-        page = max(1, min(page, total_pages))
-        start = (page - 1) * self.page_size
-        end = start + self.page_size
-        records = list(self.data.values())[start:end]
-        return records
+    def __iter__(self):
+        self._index = 0
+        return self
+
+    def __next__(self):
+        records = list(self.data.values())
+        if self._index < len(records):
+            start = self._index
+            self._index += self._page_size
+            return records[start:self._index]
+        raise StopIteration
 
 def input_error(func):
     def wrapper(*args, **kwargs):
@@ -108,8 +106,8 @@ def input_error(func):
             return func(*args, **kwargs)
         except KeyError:
             return "Contact not found."
-        except ValueError:
-            return "Invalid input."
+        except ValueError as e:
+            return str(e)
         except IndexError:
             return "Invalid input."
     return wrapper
@@ -118,7 +116,7 @@ contacts = AddressBook()
 
 def add_contact(name, phone, birthday=None):
     if name.isdigit():
-        return "Invalid input. Name should be a text."
+        return "Invalid input. Name should be text."
     if name in contacts:
         record = contacts[name]
         record.add_phone(phone)
@@ -130,7 +128,7 @@ def add_contact(name, phone, birthday=None):
 
 def change_contact(name, phone):
     if name.isdigit():
-        return "Invalid input. Name should be a text."
+        return "Invalid input. Name should be text."
     if name in contacts:
         record = contacts[name]
         record.edit_phone(record.phones[0].value, phone)
@@ -145,39 +143,33 @@ def get_phone_number(name):
     else:
         return "Contact not found."
 
-def show_all_contacts(page=1):
+def show_all_contacts():
     if len(contacts) == 0:
         return "No contacts found."
     else:
         result = ""
-        records = contacts.iterator(page)
-        for record in records:
-            phones = ", ".join(str(phone) for phone in record.phones)
-            result += f"{record.name}: {phones}\n"
+        for records in contacts:
+            for record in records:
+                phones = ", ".join(str(phone) for phone in record.phones)
+                result += f"{record.name}: {phones}\n"
         return result.strip()
 
 def main():
     print("Hello! My commands: add, change, phone, show all. Type contact name without a space. Example: add MarkoMark 138238932832. To finish type close or exit.")
     while True:
         command = input("> ").lower()
-
+        
         if command == "hello":
             print("How can I help you?")
         elif command.startswith("add"):
-            _, name, phone, *birthday = command.split(" ", 3)
-            print(add_contact(name, phone, *birthday))
+            _, name, phone = command.split(" ", 2)
+            print(add_contact(name, phone))
         elif command.startswith("change"):
             _, name, phone = command.split(" ", 2)
             print(change_contact(name, phone))
         elif command.startswith("phone"):
             _, name = command.split(" ", 1)
             print(get_phone_number(name))
-        elif command.startswith("show all"):
-            if len(command.split()) > 2:
-                _, page = command.split(" ", 2)
-                print(show_all_contacts(int(page)))
-            else:
-                print(show_all_contacts())
         elif command.startswith("days"):
             _, name = command.split(" ", 1)
             if name in contacts:
@@ -189,7 +181,8 @@ def main():
                     print(f"Days to birthday: {days}")
             else:
                 print("Contact not found.")
- 
+        elif command == "show all":
+            print(show_all_contacts())
         elif command in ["good bye", "close", "exit"]:
             print("Good bye!")
             break
